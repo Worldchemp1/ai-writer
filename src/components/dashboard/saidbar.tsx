276229f5090883b022c733fb47100cf58867e363
Worldchemp1 <worldchemp1@gmail.com>
2025-07-09 20:40:00 +0500

    feat/38: UI Layout has been created

---
+++ src/components/dashboard/saidbar.tsx
@@ -0,0 +1,48 @@
+import { Pencil } from "lucide-react";
+import PromptHistory from "./prompt-history";
+import type { TPromptHistory } from "@/shared/types/prompt-history.type";
+
+const mockItems: TPromptHistory[] = [
+  {
+    date: "Today",
+    links: [
+      {
+        title: "Prompt 1",
+        url: "/dashboard/prompt/1",
+      },
+      {
+        title: "Prompt 2",
+        url: "/dashboard/prompt/2",
+      },
+    ],
+  },
+  {
+    date: "Yesterday",
+    links: [
+      {
+        title: "Prompt 2",
+        url: "/dashboard/prompt/1",
+      },
+      {
+        title: "Prompt 2",
+        url: "/dashboard/prompt/2",
+      },
+    ],
+  },
+];
+
+export default function Saidbar() {
+  return (
+    <nav className="h-screen w-80 border-r p-4">
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold ">AI-WRITER</h1>
+        <button>
+          <Pencil size={24} />
+        </button>
+      </div>
+      <PromptHistory items={mockItems} />
+    </nav>
+  );
+}
+
+// 15:00
---
+++ src/components/layouts/dashboard-layout.tsx
@@ -0,0 +1,17 @@
+import Saidbar from "../dashboard/saidbar";
+import Navbar from "../dashboard/navbar";
+import { Outlet } from "react-router-dom";
+
+export default function DashboardLayout() {
+  return (
+    <div className="h-screen overflow-hidden flex">
+      <Saidbar />
+      <div className="w-full">
+        <Navbar />
+        <div className="p-4"><Outlet /></div>
+      </div>
+    </div>
+  );
+}
+
+// 20:00
